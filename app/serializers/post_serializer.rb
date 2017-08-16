a568a14c53621d016bff3c7d95a6daf38080ccd8
edwincarbajal <exaviercarbajal@gmail.com>
2017-08-16 00:48:02 -0400

    Add associations to serializers

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :position, :description, :employer, :location, :category
+
+  belongs_to :user, key: "author"
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :full_name, :email
 
+  has_many :posts
+
   def full_name
     "#{object.first_name} #{object.last_name}"
   end
